171c71f8684d3bde5eeb31b66abd086a0b4a1b69
Mehdi M <hi@mehdi.cc>
2020-06-06 22:09:58 +0200

    Clean Wallet output

--- src/js/modules/Block/Wallet.js
+++ src/js/modules/Block/Wallet.js
@@ -5,16 +5,24 @@ import { SOUNDS, CSS } from './config'
 
 export default class Wallet {
 
+  get coins() {
+    return this.money
+  }
+
+  set coins(value) {
+    this.money = value
+    this.toBank(value)
+  }
+
   /**
    * Constructor
    */
   constructor() {
-
-    this.scoreBoardReady = false
     this.makeSounds(true)
 
     this.fromBank().then(money => {
       this.coins = money
+      this.prepareDisplay()
       this.display()
     })
   }
@@ -24,41 +32,29 @@ export default class Wallet {
    */
   add(coins = 1) {
     this.coins++
-    this.toBank(this.coins) // save coins
 
-    Sfx.play(this.coin, .1)
-
-    const is1UP = this.coins % 100 === 0
-    if (is1UP) {
-      Sfx.play(this.oneUp, .2) // 1-UP 🍄
-    }
-
+    this.playSounds()
     this.display()
-    this.makeSounds(is1UP)
   }
 
   /**
    * Show coins amount
    */
   display() {
-    this.prepareDisplay()
     this.output.innerHTML = this.coins
-    this.pageTitle.innerHTML = 'x ' + this.coins
+    this.pageTitle.innerHTML = `x ${this.coins} • Sunappu ⍰`
   }
 
   /**
    * Prepare money count display
    */
   prepareDisplay() {
-    if (this.scoreBoardReady) { return }
-
     this.output = document.getElementById(CSS.coinsNumber)
     this.pageTitle = document.head.querySelector('title')
 
-    document.dispatchEvent(new CustomEvent('firstWalletDisplay'))
-
     document.getElementById('wallet').classList.add('wallet--on')
-    this.scoreBoardReady = true
+
+    document.dispatchEvent(new CustomEvent('walletDisplayReady'))
   }
 
   /**
@@ -79,10 +75,23 @@ export default class Wallet {
     return await idbGet('coins-amount').then(money => Number.isInteger(money) ? money : 0)
   }
 
-  makeSounds(is1UP = null) {
+  // SFX
+
+  playSounds() {
+    Sfx.play(this.coin, .1)
+
+    const isOneUp = this.coins % 100 === 0
+    if (isOneUp) {
+      Sfx.play(this.oneUp, .2) // 1-UP 🍄
+    }
+
+    this.makeSounds(isOneUp)
+  }
+
+  makeSounds(isOneUp = null) {
     this.coin = Sfx.makeFrom(SOUNDS.coin)
 
-    if (is1UP) {
+    if (isOneUp) {
       this.oneUp = Sfx.makeFrom(SOUNDS.oneUp)
     }
   }
