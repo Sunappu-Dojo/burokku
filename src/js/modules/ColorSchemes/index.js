c387c90e4501642c2b2ab5b1c829ac0e801246d9
Mehdi M <hi@mehdi.cc>
2020-06-07 02:45:01 +0200

    Initialize theme color on app launch

--- src/js/modules/ColorSchemes/index.js
+++ src/js/modules/ColorSchemes/index.js
@@ -1,15 +1,20 @@
-const DEFAULT_THEME = 'dark';
+const DEFAULT_THEME = 'light';
 const THEME_MQ = `(prefers-color-scheme: ${DEFAULT_THEME})`;
 
 export default class ColorScheme {
   constructor() {
     this.MQ = matchMedia(THEME_MQ)
-
     this.themeColor = document.head.querySelector('meta[name="theme-color"]')
 
+    this.setThemeColor()
+
     this.MQ.addListener(e => {
-      const color = getComputedStyle(document.documentElement).getPropertyValue('--bg').trim()
-      this.themeColor.setAttribute('content', color)
+      this.setThemeColor()
     })
+  }
+
+  setThemeColor() {
+    const color = getComputedStyle(document.documentElement).getPropertyValue('--bg').trim()
+    this.themeColor.setAttribute('content', color)
   }
 }
