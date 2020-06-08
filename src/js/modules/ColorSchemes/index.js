e922361f3058d6ee77246327cccf0faad9f62165
Mehdi <9340937+meduzen@users.noreply.github.com>
2020-06-08 05:03:16 +0200

    Update app icons when color scheme changes
    Also animate SVG favicons.

--- src/js/modules/ColorSchemes/index.js
+++ src/js/modules/ColorSchemes/index.js
@@ -1,20 +1,76 @@
-const DEFAULT_THEME = 'light';
-const THEME_MQ = `(prefers-color-scheme: ${DEFAULT_THEME})`;
+const head = document.head
 
-export default class ColorScheme {
-  constructor() {
-    this.MQ = matchMedia(THEME_MQ)
-    this.themeColor = document.head.querySelector('meta[name="theme-color"]')
+const themeColor = head.querySelector('meta[name="theme-color"]')
 
-    this.setThemeColor()
+// All app icons with a prefers-color-scheme media query
+const appIcons = Array.from(head.querySelectorAll('link[rel*="icon"][media]'))
 
-    this.MQ.addListener(e => {
-      this.setThemeColor()
-    })
-  }
+// Icons, categorized by color schemes.
+const groupedIcons = []
 
-  setThemeColor() {
-    const color = getComputedStyle(document.documentElement).getPropertyValue('--bg').trim()
-    this.themeColor.setAttribute('content', color)
-  }
+/**
+ * Create a list (array) of unique MediaQueryList.
+ */
+const getColorSchemesMQFrom = elems => Array.from(new Set(elems.map(el => el.media))).map(media => matchMedia(media))
+
+/**
+ * Extract color scheme name from MQ
+ *
+ * Example: '(prefers-color-scheme: light)' returns 'light'.
+ */
+const getSchemeName = media => media
+  .replace('(prefers-color-scheme: ', '')
+  .replace(')', '')
+
+
+/**
+ * Push app icons at end of <head>, telling the browser to pick among them.
+ */
+const setFavicon = icons => icons.forEach(icon => {
+  icon.remove()
+  head.insertAdjacentElement('beforeEnd', icon)
+})
+
+/**
+ * Update theme colors
+ */
+const setThemeColor = () => {
+  const color = getComputedStyle(document.documentElement).getPropertyValue('--bg').trim()
+  themeColor.setAttribute('content', color)
 }
+
+/**
+ * Update UI, fire event.
+ */
+const onColorSchemeChange = e => {
+  if (!e.matches) { return }
+
+  setFavicon(groupedIcons[e.media])
+  setThemeColor()
+
+  document.dispatchEvent(new CustomEvent('colorSchemeChange', {
+    detail: getSchemeName(e.media)
+  }))
+}
+
+/**
+ * Prepare UI and watch color scheme change.
+ */
+const watchColorSchemes = () => {
+  getColorSchemesMQFrom(appIcons).forEach(schemeMQ => {
+
+    // Gather icons by color scheme.
+    groupedIcons[schemeMQ.media] = appIcons.filter(icon => icon.media == schemeMQ.media)
+
+    // Listen to color scheme changes.
+    schemeMQ.addListener(onColorSchemeChange)
+
+    // Initialize theme color and favicons.
+    setThemeColor()
+    if (schemeMQ.matches) {
+      setFavicon(groupedIcons[schemeMQ.media])
+    }
+  })
+}
+
+export default watchColorSchemes
