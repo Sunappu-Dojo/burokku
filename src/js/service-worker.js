59b39fcfc6bfaef9be360d97a20cd54400f8f251
Mehdi M <hi@mehdi.cc>
2020-08-16 23:31:20 +0200

    Normalize sounds

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -2,7 +2,7 @@
  * Service worker strategy: network first, fallback on cache.
  */
 
-const SW_VERSION = '2.0'
+const SW_VERSION = '2.1'
 const NETWORK_TIMEOUT = 700
 const resourcesCacheKey = `cache-v${SW_VERSION}`
 
