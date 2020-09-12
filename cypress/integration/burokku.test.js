8e508597c0cd0d9063e353787031185ffd1f274f
Mehdi M <hi@mehdi.cc>
2020-09-12 02:07:46 +0200

    Add some pending tests

--- cypress/integration/burokku.test.js
+++ cypress/integration/burokku.test.js
@@ -7,9 +7,13 @@ describe('Initialization', () => {
 
   it('has at least one block', () => { cy.contains('Jump!') })
 
+  it('has only one visible block')
+
   it('has an empty wallet', () => cy.get('.wallet__amount').then(el =>
     expect(walletValue(el)).to.be.equal(0)
   ))
+
+  it('has no visible navigation arrows')
 })
 
 describe('Block', () => {
@@ -18,4 +22,16 @@ describe('Block', () => {
   it('puts a coin in the wallet', () => cy.get('.wallet__amount').then(el =>
     expect(walletValue(el)).to.be.equal(1)
   ))
+
+  it('plays a sound when the block is hit')
+})
+
+describe('Wallet', () => {
+  it('unlocks a block when reaching a threshold')
+  it('does not unlock a block if all blocks are unlocked')
+})
+
+describe('Navigation', () => {
+  it('doesn’t select another block when navigation arrows are not available')
+  it('selects another block when hitting a navigation arrow')
 })
