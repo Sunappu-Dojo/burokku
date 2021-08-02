05f1381daf432f36446aa1106482fa57138a016b
Mehdi M <hi@mehdi.cc>
2021-08-02 23:48:36 +0200

    Bump Service Worker version

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -2,7 +2,7 @@
  * Service worker strategy: network first, fallback on cache.
  */
 
-const SW_VERSION = '2.4'
+const SW_VERSION = '2.5'
 const NETWORK_TIMEOUT = 700
 const resourcesCacheKey = `cache-v${SW_VERSION}`
 
