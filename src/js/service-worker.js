1a67e6b808b5b51d7467607bcb1b5c0036c32400
Mehdi <9340937+meduzen@users.noreply.github.com>
2022-09-02 11:56:58 +0200

    Fix unreachable top bar
    This prevents the PWA install button to be used with a mouse.

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -2,7 +2,7 @@
  * Service worker strategy: network first, fallback on cache.
  */
 
-const SW_VERSION = '2.8'
+const SW_VERSION = '2.9'
 const NETWORK_TIMEOUT = 700
 
 const resourcesCacheKey = `cache-v${SW_VERSION}`
