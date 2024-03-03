074f9cfe6f654e9a3f8e5ce7a3e0cc97cb9b4d25
Mehdi M <hi@mehdi.cc>
2024-03-03 01:59:48 +0100

    Update configuration to drop Sass

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,17 +1,26 @@
 import postcssPresetEnv from 'postcss-preset-env'
+import postcssDarkThemeClass from 'postcss-dark-theme-class'
 import postcssSize from 'postcss-size'
 import cssNano from 'cssnano'
 import purgecss from '@fullhuman/postcss-purgecss'
 
 const postcssPresetEnvOptions = {
   stage: 0,
+
+  // https://github.com/csstools/postcss-plugins/blob/main/plugin-packs/postcss-preset-env/src/plugins/plugins-by-id.mjs
   features: {
-    // https://github.com/csstools/postcss-plugins/blob/main/plugin-packs/postcss-preset-env/src/plugins/plugins-by-id.mjs
+
+    // https://github.com/maximkoretskiy/postcss-initial
     'all-property': false,
+
+    // https://github.com/csstools/postcss-plugins/tree/main/plugins/postcss-color-functional-notation
     'color-functional-notation': false,
 
     // https://github.com/csstools/postcss-plugins/tree/main/plugins/postcss-nested-calc#options
     'nested-calc': { preserve: false },
+
+    // https://github.com/csstools/postcss-plugins/tree/main/plugins/postcss-nesting
+    'nesting-rules': true
   },
 }
 
@@ -26,9 +35,21 @@ const purgeCssOptions = {
 }
 
 export default ({ options, env }) => ({
+  /**
+   * The SCSS parser is used to allow inline comments in CSS files and to avoid
+   * crashes on some unknown keywords like `@container`.
+   */
+  parser: 'postcss-scss',
   plugins: [
     postcssSize(),
     postcssPresetEnv(postcssPresetEnvOptions),
+
+    // must be after `postcss-preset-env`
+    postcssDarkThemeClass({
+      darkSelector: '.theme-set.basic-black',
+      lightSelector: '.theme-set.basic-white',
+    }),
+
     env === 'production' ? cssNano(cssNanoOptions) : false,
     env === 'production' ? purgecss(purgeCssOptions) : false,
   ],
