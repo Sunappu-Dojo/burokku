f3f70c66b90e68c0e1a17583733c93b0516945c5
Mehdi M <hi@mehdi.cc>
2024-04-04 20:08:00 +0200

    Add support for local HTTPS

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { resolve } from 'node:path'
+import { homedir } from 'node:os'
 import { env } from 'node:process'
 import { defineConfig } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
@@ -39,6 +40,24 @@ const singleFileOptions = {
   inlinePattern: [
     'js/helpers/idbDetect.js',
   ],
+}
+
+/**
+ * HTTPS, works well with Laravel Valet (macOS)
+ * Laravel Valet HTTPS: https://github.com/laravel/docs/blob/master/valet.md#securing-sites-with-tls
+ * Not tested with other setups.
+ */
+
+const host = env?.SERVER_HOST ?? null
+let https = env?.SERVER_HTTPS === 'true'
+
+if (https && host && env.SERVER_CERTIFICATES_DIR) {
+  const certificatesPath = `${homedir()}/${env.SERVER_CERTIFICATES_DIR}/${host}`
+
+  https = {
+    key: `${certificatesPath}.key`,
+    cert: `${certificatesPath}.crt`,
+  }
 }
 
 export default defineConfig({
@@ -96,6 +115,8 @@ export default defineConfig({
   ],
 
   server: {
-    open: env?.BROWSER_OPEN == 'true',
+    open: env?.BROWSER_OPEN === 'true',
+    ...(host ? { host } : null),
+    https,
   },
 })
